f0d61c60392e9d54f4efd91c1e49092f9c5e9b1d
NiranjithCU <108677542+NiranjithCU@users.noreply.github.com>
2023-02-28 17:39:59 +0000

    Deploying to gh-pages from @ NiranjithCU/rds-tailwind-vite@f54d65e1e03432bd71e132b11f1450d623110938 🚀

--- assets/preview-b84a2407.js
+++
@@ -1,2 +0,0 @@
-const o={backgrounds:{default:"light"},actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}}};export{o as parameters};
-//# sourceMappingURL=preview-b84a2407.js.map
